862c9fc0f4f5f8d1af786784881608b15a24e684
SouravKB <souravkannanthab3219@gmail.com>
2021-12-16 12:13:48 +0530

    Fixed bug in Sqfl model generator.

--- lib/src/generators/sqfl_model_generator.dart
+++ lib/src/generators/sqfl_model_generator.dart
@@ -1,9 +1,11 @@
 import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/nullability_suffix.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:model_annotations/model_annotations.dart';
 import 'package:model_helper/src/util/sqfl_model_contents.dart';
 import 'package:model_helper/src/util/string_extensions.dart';
 import 'package:model_helper/src/util/type_extensions.dart';
+import 'package:model_helper/src/util/type_suffix_extension.dart';
 import 'package:source_gen/source_gen.dart';
 
 class SqflModelGenerator extends GeneratorForAnnotation<SqflModel> {
@@ -40,25 +42,8 @@ class SqflModelGenerator extends GeneratorForAnnotation<SqflModel> {
       field.colField = "col${field.name.capitalize()}";
       code.writeln('  static const ${field.colField} = "${field.name}";');
 
-      if (!field.type.isValidType())
-        throw Exception('${model.modelType} cannot contain variable of type ${field.type}');
-
-      String sqflType;
-      if (field.type.isDartCoreInt || field.type.isDartCoreBool)
-        sqflType = 'INTEGER';
-      else if (field.type.isDartCoreNum || field.type.isDartCoreDouble)
-        sqflType = 'REAL';
-      else if (field.type.isDartCoreString || field.type.isDateTime)
-        sqflType = 'TEXT';
-      else if (field.type.isUint8List)
-        sqflType = 'BLOB';
-      else
-        throw Exception('This should never execute!');
-
-      if (field.type.nullabilitySuffix.index == 2) // NullabilitySuffix.none(2);
-        sqflType += ' NOT NULL';
-
-      code.writeln('  static const typeOf${field.name.capitalize()} = "${sqflType}";');
+      code.writeln(
+          '  static const typeOf${field.name.capitalize()} = "${_getSqflType(field.type)}";');
     });
 
     return code.toString();
@@ -69,14 +54,7 @@ class SqflModelGenerator extends GeneratorForAnnotation<SqflModel> {
     code.writeln('Map<String, Object?> toMap() => {');
 
     model.fields.forEach((field) {
-      if (field.type.isDartCoreBool)
-        code.writeln('  ${field.colField}: ${field.name} ? 1 : 0,');
-      else if (field.type.isDateTime) if (field.type.nullabilitySuffix.index == 2)
-        code.writeln('  ${field.colField}: ${field.name}.toIso8601String(),');
-      else
-        code.writeln('  ${field.colField}: ${field.name}?.toIso8601String(),');
-      else
-        code.writeln('  ${field.colField}: ${field.name},');
+      code.writeln('  ${field.colField}: ${_getToMapString(field.type, field.name)},');
     });
 
     code.writeln('};');
@@ -89,28 +67,67 @@ class SqflModelGenerator extends GeneratorForAnnotation<SqflModel> {
         'static ${model.cls.thisType} fromMap(Map<String, Object?> map) => ${model.cls.thisType}(');
 
     model.fields.forEach((field) {
-      if (field.type.isDartCoreBool)
-        code.writeln('  ${field.name}: map[${field.colField}] == 1,');
-      else if (field.type.isDateTime)
-        code.writeln('  ${field.name}: DateTime.parse(map[${field.colField}] as String),');
-      else
-        code.writeln('  ${field.name}: map[${field.colField}] as ${field.type},');
+      final fieldName = 'map[${field.colField}]';
+      code.writeln('  ${field.name}: ${_getFromMapString(field.type, fieldName)},');
     });
 
     code.writeln(');');
     return code.toString();
   }
-}
 
-extension _ValidTypes on DartType {
-  bool isValidType() {
-    return isDartCoreNum ||
-        isDartCoreInt ||
-        isDartCoreDouble ||
-        isDartCoreBool ||
-        isDartCoreString ||
-        isUint8List ||
-        isDateTime;
+  String _getSqflType(DartType type) {
+    final nullability = type.nullabilitySuffix == NullabilitySuffix.none ? ' NOT NULL' : '';
+
+    if (type.isDartCoreInt || type.isDartCoreBool || type.isDateTime) {
+      return 'INTEGER' + nullability;
+    }
+    if (type.isDartCoreNum || type.isDartCoreDouble) {
+      return 'REAL' + nullability;
+    }
+    if (type.isDartCoreString) {
+      return 'TEXT' + nullability;
+    }
+    if (type.isUint8List) {
+      return 'BLOB' + nullability;
+    }
+    throw Exception('Invalid type "$type" in class annotated with "CfsModel" annotation.');
+  }
+
+  String _getToMapString(DartType type, String field) {
+    if (type.isDartCoreInt ||
+        type.isDartCoreDouble ||
+        type.isDartCoreNum ||
+        type.isDartCoreString ||
+        type.isUint8List) {
+      return '$field';
+    }
+    if (type.isDartCoreBool) {
+      return '$field ? 1 : 0';
+    }
+    if (type.isDateTime) {
+      return '$field${type.nullSuffix}.millisecondsSinceEpoch';
+    }
+    throw Exception('Invalid type "$type" in class annotated with "CfsModel" annotation.');
+  }
+
+  String _getFromMapString(DartType type, String field) {
+    if (type.isDartCoreInt ||
+        type.isDartCoreDouble ||
+        type.isDartCoreNum ||
+        type.isDartCoreString ||
+        type.isUint8List) {
+      return '$field as $type';
+    }
+    if (type.isDartCoreBool) {
+      return '$field == 1';
+    }
+    if (type.isDateTime) {
+      if (type.nullabilitySuffix == NullabilitySuffix.none)
+        return 'DateTime.fromMillisecondsSinceEpoch($field as int)';
+      else
+        return '$field != null ? DateTime.fromMillisecondsSinceEpoch($field as int) : null';
+    }
+    throw Exception('Invalid type "$type" in class annotated with "CfsModel" annotation.');
   }
 }
 
